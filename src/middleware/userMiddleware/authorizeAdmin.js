92ce8d1cddd4ae96567f519d96a6a8cd8e280e20
Ziadhisham288 <ziadabomadina288@gmail.com>
2024-07-19 05:58:11 +0300

    fixed authorization

--- src/middleware/userMiddleware/authorizeAdmin.js
+++ src/middleware/userMiddleware/authorizeAdmin.js
@@ -1,6 +1,6 @@
 
 export const authorizeAdmin = (req, res, next) => {
-  const {role} = req.auth
+  const {role} = req.auth.user
 
   if (role != "admin") {
     return res.json({message : "Not authorized to modify books"})
